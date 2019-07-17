fca191ad30b7316a60459d46788fa82e0b76f7e6
osamtimizer <eaten.jb@gmail.com>
2019-07-18 02:13:42 +0900

    add GoogleService-info.plist(not managed on GitHub)

--- RxSwift-with-Firebase/AppDelegate.swift
+++ RxSwift-with-Firebase/AppDelegate.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -8,6 +9,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
+
+    FirebaseApp.configure()
     return true
   }
 
